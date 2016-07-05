7f3d7b2a0caf10af7876866309029db3e971b6fb
tomasmaiorino <tomasmaiorino@gmail.com>
2016-07-04 23:58:33 -0300

    order cards result

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -5,4 +5,23 @@ module HomeHelper
 	def get_card_url(card)
 		CardsUtil.new.get_card_url(card)
 	end
+
+	def sort(card)
+		model = ['2','3','4','5','6','7','8','9','10','J','Q','K', 'A']
+		return sort_cards(model, card)
+	end
+
+	def sort_cards(model, to_order)
+		n = []
+		if to_order != nil && !to_order.empty?
+			model.each_with_index do |item, index|
+		  		to_order.each_with_index do |val, ind|
+		  			if model[index.to_i] == to_order[ind][0, to_order[ind].length - 1]
+			  				n << to_order[ind]
+		  			end
+		  		end
+		  	end
+		  end
+	  	return n
+	end
 end
